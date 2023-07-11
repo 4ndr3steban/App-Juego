bf7c0488f1dee1f49ca57efc9c9cd827cbff1037
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2023-07-11 14:19:31 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from settings import settings
 from routers import router_user, router_products, router_challenge
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # API para usuarios
 app.include_router(router_user.router)
